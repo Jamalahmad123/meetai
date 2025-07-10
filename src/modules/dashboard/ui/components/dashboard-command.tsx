f34f0fd80ead8fe51c1962022423c0e2939639d0
Jamal Ahmad <jamalcs007@@gmail.com>
2025-07-10 10:39:10 +0500

    07: dashboard navbar

---
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { CommandDialog, CommandInput, CommandItem, CommandList } from '@/components/ui/command'
+import { SearchIcon } from 'lucide-react';
+
+
+const DashboardCommand = () => {
+
+  const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen(open => !open)
+      }
+    }
+
+    document.addEventListener("keydown", down)
+    return () => document.removeEventListener("keydown", down)
+  }, [])
+
+
+
+  return (
+    <>
+      <Button
+        className="h-9 w-[240px] justify-start font-normal text-muted-foreground hover:text-muted-foreground"
+        variant="outline"
+        size="sm"
+      >
+        <SearchIcon size={16} />
+        Search
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium text-muted-foreground">
+          <span className="text-xs">&#8984;K</span>
+        </kbd>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder='Find a meeting or agent' />
+        <CommandList>
+          <CommandItem>Test</CommandItem>
+        </CommandList>
+      </CommandDialog>
+    </>
+  )
+}
+
+export default DashboardCommand
