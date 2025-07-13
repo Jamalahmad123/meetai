587fa9893e1de79ac261377c7582ea1db573e71c
Jamal Ahmad <jamalcs007@@gmail.com>
2025-07-13 12:02:32 +0500

    15 agents actions

--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -1,16 +1,29 @@
-"use client"
+"use client";
 
-import { ColumnDef } from "@tanstack/react-table"
-import { AgentGetOne } from "../../types"
-import { GeneratedAvatar } from "@/components/generated-avatar"
-import { CornerRightDownIcon, MoreHorizontal, VideoIcon } from "lucide-react"
-import { Badge } from "@/components/ui/badge"
-import { useState } from "react"
-import { Dialog, DialogTrigger } from "@/components/ui/dialog"
-import { AlertDialog, AlertDialogTrigger } from "@/components/ui/alert-dialog"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import Link from "next/link";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { CornerRightDownIcon, MoreHorizontal, VideoIcon } from "lucide-react";
+
+import { AgentGetOne } from "../../types";
+
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { Badge } from "@/components/ui/badge";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { AlertDialog, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { DeleteAgentAlert } from "./delete-agent-alert";
+import { EditAgentDialog } from "./edit-agent-dialog";
 
 export const columns: ColumnDef<AgentGetOne>[] = [
   {
@@ -19,41 +32,44 @@ export const columns: ColumnDef<AgentGetOne>[] = [
     cell: ({ row }) => (
       <div className="flex flex-col gap-y-1">
         <div className="flex items-center gap-x-2">
-          <GeneratedAvatar variant="botttsNeutral" seed={row.original.name} className="size-6" />
+          <GeneratedAvatar
+            variant="botttsNeutral"
+            seed={row.original.name}
+            className="size-6"
+          />
           <span className="font-semibold capitalize">{row.original.name}</span>
         </div>
         <div className="flex items-center gap-x-2">
           <CornerRightDownIcon className="size-3 text-muted-foreground" />
-          <p className="text-sm truncate capitalize text-muted-foreground max-w-52">{row.original.instructions}</p>
+          <p className="text-sm truncate capitalize text-muted-foreground max-w-52">
+            {row.original.instructions}
+          </p>
         </div>
       </div>
-    )
+    ),
   },
   {
     accessorKey: "meetingCount",
     header: "Meetings",
     cell: ({ row }) => (
-      <Badge variant="outline" className="flex items-center gap-x-2 [&>svg]:size-4">
+      <Badge
+        variant="outline"
+        className="flex items-center gap-x-2 [&>svg]:size-4"
+      >
         <VideoIcon className="text-blue-700" />
-        {row.original.meetingCount} {row.original.meetingCount === 1 ? "meeting" : "meetings"}
+        {row.original.meetingCount}{" "}
+        {row.original.meetingCount === 1 ? "meeting" : "meetings"}
       </Badge>
-    )
+    ),
   },
   {
     id: "actions",
     cell: ({ row }) => <ActionsRow agent={row.original} />,
     size: 40,
   },
-]
-
-type DialogType = "None" | "Delete" | "Edit"
+];
 
 const ActionsRow = ({ agent }: { agent: AgentGetOne }) => {
-  const [dialogType, setDialogType] = useState<DialogType>("None");
-
-  const handleDialogType = (type: DialogType) => setDialogType(type);
-
-
   return (
     <Dialog>
       <AlertDialog>
@@ -68,26 +84,22 @@ const ActionsRow = ({ agent }: { agent: AgentGetOne }) => {
             <DropdownMenuLabel>Actions</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem asChild>
-              <Link href={`/agents/${agent.id}`}>
-                View
-              </Link>
+              <Link href={`/agents/${agent.id}`}>View</Link>
             </DropdownMenuItem>
             <DialogTrigger asChild>
-              <DropdownMenuItem onClick={() => handleDialogType("Edit")}>
-                Edit
-              </DropdownMenuItem>
+              <DropdownMenuItem>Edit</DropdownMenuItem>
             </DialogTrigger>
             <AlertDialogTrigger asChild>
-              <DropdownMenuItem onClick={() => handleDialogType("Delete")}>
+              <DropdownMenuItem>
                 Delete
                 <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
               </DropdownMenuItem>
             </AlertDialogTrigger>
           </DropdownMenuContent>
         </DropdownMenu>
-        {/* TODO: Delete alert */}
+        <DeleteAgentAlert agentId={agent.id} />
       </AlertDialog>
-      {/* TODO: Edit dialog */}
+      <EditAgentDialog agent={agent} />
     </Dialog>
   );
 };
---
+++ src/modules/agents/ui/components/delete-agent-alert.tsx
@@ -0,0 +1,50 @@
+import { AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner";
+
+
+
+export const DeleteAgentAlert = ({ agentId, isViewPage }: { agentId: string; isViewPage?: boolean }) => {
+
+  const router = useRouter()
+  const trpc = useTRPC();
+
+  const queryClient = useQueryClient()
+
+  const removeAgent = useMutation(
+    trpc.agents.remove.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(trpc.agents.getMany.queryOptions())
+
+        // TODO: invilidate free tier usage
+
+        if (isViewPage) {
+          router.push("/agents")
+        }
+      },
+      onError: (error) => {
+        toast.error(error.message)
+      }
+    })
+  )
+
+
+  return (
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you sure you want to delete this agent?</AlertDialogTitle>
+        <AlertDialogDescription>This action cannot be undone. The agent and all related data will be permanently removed.</AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction onClick={() => removeAgent.mutate({
+          id: agentId
+        })}>
+          Confirm
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  )
+}
